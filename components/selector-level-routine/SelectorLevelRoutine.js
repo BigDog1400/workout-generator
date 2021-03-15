40fc0d88b7df2fdceccaa7368b08e19f99923212
Isaac Peña <isaac.pr99@gmail.com>
2021-03-14 21:31:54 -0400

    feat: Agregar constant array con valores de distintos niveles

--- components/selector-level-routine/SelectorLevelRoutine.js
+++ components/selector-level-routine/SelectorLevelRoutine.js
@@ -1,44 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./styles.module.scss";
 import { connect } from "react-redux";
 import { changeType } from "../../redux/type/action";
-const SelectorLevelRoutine = ({ typeTraining, changeType, handleOnChange }) => {
-  console.log(typeTraining);
+import { levelTrainings } from "../../constants/levelTrainings";
+
+const SelectorLevelRoutine = ({ typeTrainingSelected, changeType }) => {
   const handleChangeDifficult = (type) => {
-    console.log(type, "[dispatch]");
     changeType(type);
   };
   return (
     <div className={styles.SelectorLevelRoutine}>
-      <div className={styles.SelectorLevelRoutineLevel}>
-        <FontAwesomeIcon
-          icon={faChevronRight}
-          className={styles.SelectorLevelRoutineLevel__labelIcon}
-        />
-        <span
-          className={styles.SelectorLevelRoutineLevel__label}
-          onClick={() => handleChangeDifficult("easy")}
-        >
-          FACIL
-        </span>
-      </div>
-      <div className={styles.SelectorLevelRoutineLevel}>
-        <span className={styles.SelectorLevelRoutineLevel__label}>
-          INTERMEDIO
-        </span>
-      </div>
-      <div className={styles.SelectorLevelRoutineLevel}>
-        <span className={styles.SelectorLevelRoutineLevel__label}>DIFICIL</span>
-      </div>
+      {levelTrainings.map((level) => (
+        <div className={styles.SelectorLevelRoutineLevel}>
+          {typeTrainingSelected === level.name ? (
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className={styles.SelectorLevelRoutineLevel__labelIcon}
+            />
+          ) : null}
+          <span
+            className={styles.SelectorLevelRoutineLevel__label}
+            onClick={() => handleChangeDifficult(level.name)}
+          >
+            {level.label}
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
 
 const mapStateToProps = ({ typeTraining }) => {
   return {
-    typeTraining: typeTraining
+    typeTrainingSelected: typeTraining.type
   };
 };
 
---
+++ constants/levelTrainings.js
@@ -0,0 +1,14 @@
+export const levelTrainings = [
+  {
+    name: "easy",
+    label: "Fácil"
+  },
+  {
+    name: "intermediate",
+    label: "Intermedio"
+  },
+  {
+    name: "hard",
+    label: "Difícil"
+  }
+];
