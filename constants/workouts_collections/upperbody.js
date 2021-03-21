18a2d66b6b235016b4f3a8c3bb45c680cf7a4f50
Isaac Peña <isaac.pr99@gmail.com>
2021-03-20 22:07:25 -0400

    feat: Agregar lista de ejercicios iniciales

---
+++ constants/workouts_collections/upperbody.js
@@ -0,0 +1,153 @@
+export const easy_upperbody = [
+  {
+    name: "Push ups",
+    unit: "reps",
+    amount: 15
+  },
+  {
+    name: "Tríceps extensions",
+    unit: "reps",
+    amount: 15
+  },
+  {
+    name: "Beginner Skull Crushers",
+    unit: "reps",
+    amount: 10
+  },
+  {
+    name: "Low Plank To High Plank",
+    unit: "seconds",
+    amount: 30
+  },
+  {
+    name: "Bench Dips",
+    unit: "reps",
+    amount: 15
+  },
+  {
+    name: "Explosive Knee Push Ups",
+    unit: "reps",
+    amount: 12
+  },
+  {
+    name: "Bar Curls",
+    unit: "reps",
+    amount: 10
+  },
+  {
+    name: "Bar Curls",
+    unit: "seconds",
+    amount: 0
+  },
+  {
+    name: "Jump Pull Ups",
+    unit: "reps",
+    amount: 10
+  },
+  {
+    name: "Australian Pull Up ",
+    unit: "reps",
+    amount: 15
+  },
+  {
+    name: "Half Burpees ",
+    unit: "seconds",
+    amount: 60
+  },
+  {
+    name: "Handstand Hold Against The Wall",
+    unit: "seconds",
+    amount: 0
+  },
+  {
+    name: "Straight Arm Plank Lean",
+    unit: "reps",
+    amount: 10
+  },
+  {
+    name: "Shoulder To Shoulder",
+    unit: "seconds",
+    amount: 60
+  },
+  {
+    name: "Pike Push Up + Hold",
+    unit: "reps",
+    amount: 10
+  },
+  {
+    name: "Handstand Kick Ups",
+    unit: "reps",
+    amount: 5
+  },
+  { name: "Planche Lean Hold", unit: "seconds", amount: 0 },
+  { name: "Dips", unit: "reps", amount: 10 },
+  { name: "Hanging Knee Raises", unit: "reps", amount: 15 },
+  { name: "Jump Chin Ups", unit: "reps", amount: 10 },
+  { name: "Incline Push Ups", unit: "reps", amount: 15 },
+  { name: "Beginner Skull Crushers (Close Grip)", unit: "reps", amount: 10 },
+  { name: "Slow Push Ups", unit: "reps", amount: 12 },
+  { name: "Chin Up Hold", unit: "time", amount: 0 },
+  { name: "Toe Touches", unit: "reps", amount: 15 },
+  { name: "Tuck Front Lever Hold", unit: "seconds", amount: 0 }
+];
+
+export const intermediate_upperbody = [
+  { name: "Elevated Shoulder Taps", unit: "seconds", amount: 30 },
+  { name: "Pike Walks Across", unit: "seconds", amount: 30 },
+  { name: "Handstand Push Ups (Against The Wall)", unit: "reps", amount: 7 },
+  { name: "Elevated Pike Push Up", unit: "reps", amount: 10 },
+  { name: "Pike Push Up", unit: "reps", amount: 12 },
+  { name: "Wall Walks", unit: "reps", amount: 5 },
+  { name: "Wide Push Ups", unit: "reps", amount: 15 },
+  { name: "Straight Bar Dips (Supinated Grip)", unit: "reps", amount: 15 },
+  { name: "Close Tricep Extension", unit: "reps", amount: 12 },
+  { name: "Archer Push Ups", unit: "reps", amount: 8 },
+  { name: "Archer Push Ups", unit: "reps", amount: 8 },
+  { name: "Diamond Push Ups", unit: "reps", amount: 15 },
+  { name: "Dips", unit: "reps", amount: 15 },
+  { name: "Skull Crushers (Advanced)", unit: "reps", amount: 12 },
+  { name: "Commando Pull Up", unit: "reps", amount: 5 },
+  { name: "Chin Ups (Wide Grip)", unit: "reps", amount: 12 },
+  { name: "Negative Muscle Up", unit: "reps", amount: 6 },
+  { name: "Hanging Scapula Shrugs", unit: "reps", amount: 12 },
+  { name: "Pseudo Push Ups", unit: "reps", amount: 12 },
+  { name: "Tuck Front Lever Raises", unit: "reps", amount: 10 },
+  { name: "Explosive Negative Push Ups", unit: "reps", amount: 12 },
+  { name: "Straight Bar Dips", unit: "reps", amount: 15 },
+  { name: "Explosive Dips", unit: "reps", amount: 12 },
+  { name: "Handstand Press Negative", unit: "reps", amount: 5 }
+];
+
+export const advanced_upperbody = [
+  { name: "L Sit Hold (On Dip Bars)", unit: "seconds", amount: 30 },
+  { name: "Tuck Planche Push Ups (On Dip Bar)", unit: "reps", amount: 8 },
+  { name: "L Sit + Straddle Planche (Dip Bars)", unit: "reps", amount: 5 },
+  { name: "Handstand Push Ups (On Parallettes)", unit: "reps", amount: 5 },
+  { name: "Pike Walks Across", unit: "seconds", amount: 25 },
+  { name: "Handstand Push Ups (Against The Wall)", unit: "reps", amount: 8 },
+  { name: "Elevated Shoulder Taps", unit: "seconds", amount: 40 },
+  { name: "Diamond Push Ups", unit: "seconds", amount: 20 },
+  { name: "Cross Tricep Extensions", unit: "reps", amount: 15 },
+  { name: "Archer Straight Bar Dips", unit: "reps", amount: 6 },
+  { name: "Explosive Push Ups", unit: "reps", amount: 20 },
+  { name: "Skull Crushers (Advanced)", unit: "reps", amount: 15 },
+  { name: "Elevated Diamond Pike Push Up", unit: "reps", amount: 12 },
+  { name: "Archer Push Ups", unit: "reps", amount: 10 },
+  { name: "Pull Ups (Wide+Shoulder+Close Grip)", unit: "reps", amount: 6 },
+  { name: "Negative Hefesto", unit: "reps", amount: 4 },
+  { name: "Tuck Front Lever Pull Ups", unit: "reps", amount: 8 },
+  { name: "Commando Pull Up (One Arm Negative)", unit: "reps", amount: 6 },
+  { name: "Ice Cream Makers", unit: "reps", amount: 8 },
+  { name: "Explosive Negative Push Ups", unit: "reps", amount: 15 },
+  { name: "Side To Side Push Ups", unit: "seconds", amount: 30 },
+  { name: "Hanging On The Bar One Arm", unit: "seconds", amount: 20 },
+  {
+    name: "One Arm Assisted Pull Up (With Resistance Bands)",
+    unit: "reps",
+    amount: 6
+  },
+  { name: "Inverted Row Chin Ups", unit: "reps", amount: 8 },
+  { name: "High Pull Ups", unit: "reps", amount: 0 },
+  { name: "L Sit To Handstand (Parallettes)", unit: "reps", amount: 4 },
+  { name: "Full Planche Lean Hold (Dip Bars)", unit: "seconds", amount: 0 }
+];
