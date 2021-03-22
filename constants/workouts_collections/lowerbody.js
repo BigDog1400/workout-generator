396e96d320b6b6ae5e71f1b6cebbab3e13311f82
Isaac Peña <isaac.pr99@gmail.com>
2021-03-21 21:45:08 -0400

    fix: Fix typo

---
+++ constants/workouts_collections/lowerbody.js
@@ -0,0 +1,278 @@
+export const easy_lowerbody = [
+  {
+    name: "Jump Squats",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Calf Raises + Hold",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Single Leg Hamstring Bridge",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Lunges",
+    amount: "10",
+    unit: "reps"
+  },
+  {
+    name: "Box Step Up",
+    amount: "10",
+    unit: "reps"
+  },
+  {
+    name: "Wall Sit",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Alternating Side Lunges",
+    amount: "10",
+    unit: "reps"
+  },
+  {
+    name: "Bulgarian Split Squat",
+    amount: "10",
+    unit: "reps"
+  },
+  {
+    name: "Burpees",
+    amount: "6",
+    unit: "reps"
+  },
+  {
+    name: "Squats",
+    amount: "20",
+    unit: "reps"
+  },
+  {
+    name: "High Knee Taps",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Switching Lunges",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Tip Toe Wall Sit",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Crab Walks",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Side Hops",
+    amount: "30",
+    unit: "time"
+  }
+];
+
+export const intermediate_lowerbody = [
+  {
+    name: "Elevated Single Leg Glute Bridge",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Lateral Lunge + Curtsy Lunge",
+    amount: "12",
+    unit: "reps"
+  },
+  {
+    name: "Switching Lunges",
+    amount: "40",
+    unit: "time"
+  },
+  {
+    name: "Box Jumps",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Assisted Pistol Squat",
+    amount: "6",
+    unit: "reps"
+  },
+  {
+    name: "Tip Toe Wall Sit",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Toe Touches",
+    amount: "40",
+    unit: "time"
+  },
+  {
+    name: "Bulgarian Split Squat",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Burpees",
+    amount: "8",
+    unit: "reps"
+  },
+  {
+    name: "Alternating Side Lunges",
+    amount: "14",
+    unit: "reps"
+  },
+  {
+    name: "Broad Jumps",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "High Knee Taps",
+    amount: "40",
+    unit: "time"
+  },
+  {
+    name: "Calf Raises + Hold",
+    amount: "20",
+    unit: "reps"
+  },
+  {
+    name: "Wall Sit",
+    amount: "35",
+    unit: "time"
+  },
+  {
+    name: "Squats",
+    amount: "25",
+    unit: "reps"
+  },
+  {
+    name: "Jump Squats",
+    amount: "30",
+    unit: "time"
+  },
+  {
+    name: "Lunges",
+    amount: "12",
+    unit: "reps"
+  },
+  {
+    name: "Crab Walks",
+    amount: "25",
+    unit: "time"
+  },
+  {
+    name: "Box Step Up",
+    amount: "15",
+    unit: "time"
+  }
+];
+
+export const advanced_lowerbody = [
+  {
+    name: "Lateral Lunge + Curtsy Lunge",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Tip Toe Wall Sit",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Side Hops",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Single Leg Romanian Deadlift + Knee Raises",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Box Jumps",
+    amount: "20",
+    unit: "reps"
+  },
+  {
+    name: "Pistol Squats",
+    amount: "8",
+    unit: "reps"
+  },
+  {
+    name: "Bulgarian Split Squat",
+    amount: "18",
+    unit: "reps"
+  },
+  {
+    name: "Narrow Squats",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Switching Lunges",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Jump Squats (Advanced)",
+    amount: "25",
+    unit: "reps"
+  },
+  {
+    name: "Side To Side Jump Squats",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Reverse Lunges",
+    amount: "25",
+    unit: "time"
+  },
+  {
+    name: "High Knee Taps",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Squat Lunge",
+    amount: "20",
+    unit: "reps"
+  },
+  {
+    name: "Alternating Side Lunges",
+    amount: "15",
+    unit: "reps"
+  },
+  {
+    name: "Jump Squats",
+    amount: "30",
+    unit: "reps"
+  },
+  {
+    name: "Wall Sit",
+    amount: "45",
+    unit: "time"
+  },
+  {
+    name: "Broad Jumps",
+    amount: "35",
+    unit: "time"
+  },
+  {
+    name: "Burpees",
+    amount: "10",
+    unit: "reps"
+  },
+  {
+    name: "Calf Raises + Hold",
+    amount: "45",
+    unit: "time"
+  }
+];
